0e93472548bfbd917387948f58a4efa42563a249
MALRMALR <michaelalross@gmail.com>
2016-12-11 00:57:36 -0800

    updated passport middleware

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,14 @@ passport.use(new FacebookStrategy({
     // profileFields: ['id', 'displayName', 'photos', 'email'],
     // enableProof: true
   },
+  //success function
   function(accessToken, refreshToken, profile, cb) {
-    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-    console.log(profile.id);
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
   }
 ));
 
