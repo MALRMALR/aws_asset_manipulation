5cea183f62db7ef0df8505661384d258f804027b
MALRMALR <michaelalross@gmail.com>
2016-12-11 03:05:43 -0800

    passing fb params into obj

--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,14 @@ passport.use(new FacebookStrategy({
     // be associated with a user record in the application's database, which
     // allows for account linking and authentication with other identity
     // providers.
-    console.log(accessToken);
-    return cb(null, profile);
+    var user = {
+      'email': profile.emails[0].value,
+      'name' : profile.name.givenName + ' ' + profile.name.familyName,
+      'id'   : profile.id,
+      'token': accessToken
+    }
+    return done(null, user);
+
   }
 ));
 
@@ -146,7 +152,7 @@ router.get('/login/facebook',
 router.get('/login/facebook/return',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/account');
+    res.redirect('/projects');
   });
 
 router.get('/account',
