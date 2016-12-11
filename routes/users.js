47a13fa63e65085b9654bb53eacfaef3398b8ec7
MALRMALR <michaelalross@gmail.com>
2016-12-10 20:09:57 -0800

    deployment

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
