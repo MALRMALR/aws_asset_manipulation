e09ea76f8a3efa7f93fa8db82109a69cae4f038d
MALRMALR <michaelalross@gmail.com>
2016-12-12 20:37:07 -0800

    adding chat view

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -14,6 +14,7 @@ window.onload = function() {
                 html += messages[i] + '<br />';
             }
             content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
         } else {
             console.log("There is a problem:", data);
         }
