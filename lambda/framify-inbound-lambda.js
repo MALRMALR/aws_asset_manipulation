256c9d834c6834e1d8b18c0ace5ab7b02f1d4681
MALRMALR <michaelalross@gmail.com>
2016-12-24 17:21:53 -0800

    lambda fxnality

--- lambda/framify-inbound-lambda.js
+++ lambda/framify-inbound-lambda.js
@@ -21,16 +21,18 @@ var table = "demoProjectsV3";
 
 exports.handler = function(event, context) {
   console.log('received event');
-  console.log(event);
+  // console.log(event);
   var s3Params = {
-    Bucket: event.Bucket,
-    Key: event.Key
-  }
+    Bucket: 'gn-inbound',
+    Key: '/public/development/projects'
+  };
   s3.getObject(s3Params, function(err, data){
       if (err) {
         console.log(err, err.stack);
       } else {
         console.log(data);
       }
-    })
+    });
+
+
 }
